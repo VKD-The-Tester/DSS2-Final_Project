a6392f0eedbb804a6a072d1ebc961eb6d5d59839
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-16 18:50:28 +0200

    The initial properties for the TodoItem class got added

--- DSS2-Backend/DSS2-Backend/Models/TodoItem.cs
+++ DSS2-Backend/DSS2-Backend/Models/TodoItem.cs
@@ -2,6 +2,23 @@
 {
     public class TodoItem
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Details { get; set; } = null!;
+
+        public Priority Priority { get; set; }
+
+        public string DueDate { get; set; } = null!;
+
+        public bool IsCompleted { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 
     public enum Priority { low = 1, medium = 2, high = 3 }
