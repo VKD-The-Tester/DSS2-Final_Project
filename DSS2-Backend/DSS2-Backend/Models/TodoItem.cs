fa7e8b9112fd071621b5eb9c337257b6ae2b67bb
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-17 18:25:33 +0200

    Data annotations got added to the TodoItem data model.

--- DSS2-Backend/DSS2-Backend/Models/TodoItem.cs
+++ DSS2-Backend/DSS2-Backend/Models/TodoItem.cs
@@ -1,4 +1,7 @@
-﻿namespace DSS2_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DSS2_Backend.Models
 {
     public class TodoItem
     {
@@ -7,26 +10,37 @@
             this.CreatedAt = DateTime.UtcNow;
         }
 
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; } = null!;
 
+        [Required]
         public string Details { get; set; } = null!;
 
+        [Required]
         public Priority Priority { get; set; }
 
+        [Required, DataType(DataType.DateTime)]
         public DateTime DueDate { get; set; }
 
+        [Required]
         public bool IsCompleted { get; set; }
 
+        [Required]
         public bool IsPublic { get; set; }
 
+        [Required, DataType(DataType.DateTime)]
         public DateTime CreatedAt { get; private set; }
 
+        [Required, DataType(DataType.DateTime)]
         public DateTime UpdatedAt { get; set; }
 
+        [ForeignKey(nameof(UserId))]
         public virtual User User { get; set; } = null!;
 
+        [Required]
         public int UserId { get; set; }
     }
 
