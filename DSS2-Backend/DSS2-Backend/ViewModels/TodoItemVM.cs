e04e1db341d9215d0ef020180a1f15559322a6f9
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-23 21:02:36 +0200

    Added some initial properties to the TodoItemVM.

--- DSS2-Backend/DSS2-Backend/ViewModels/TodoItemVM.cs
+++ DSS2-Backend/DSS2-Backend/ViewModels/TodoItemVM.cs
@@ -1,6 +1,26 @@
-﻿namespace DSS2_Backend.ViewModels
+﻿using DSS2_Backend.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DSS2_Backend.ViewModels
 {
     public class TodoItemVM
     {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Details { get; set; } = null!;
+
+        [Required]
+        public Priority Priority { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public bool IsCompleted { get; set; }
+
+        [Required]
+        public bool IsPublic { get; set; }
     }
 }
