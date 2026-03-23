dfdc58732c57defae653327e4dc226d1525f5003
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-23 20:58:40 +0200

    TodoItemVM got added.

--- DSS2-Backend/DSS2-Backend/obj/Debug/net8.0/DSS2-Backend.AssemblyInfo.cs
+++ DSS2-Backend/DSS2-Backend/obj/Debug/net8.0/DSS2-Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSS2-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30687d3e85c1fa4926154c1443682ae8fbfb1b59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63779c3eef1c6d935dcf806d3fb66bddab786f85")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSS2-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSS2-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
