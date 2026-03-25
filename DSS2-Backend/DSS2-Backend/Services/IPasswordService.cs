53650394d71d2a8bf58087fe565f9f903c489c57
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-25 17:33:38 +0200

    The HashPassword() method is now completed.

--- DSS2-Backend/DSS2-Backend/Services/IPasswordService.cs
+++ DSS2-Backend/DSS2-Backend/Services/IPasswordService.cs
@@ -1,10 +1,11 @@
 ﻿using DSS2_Backend.Dtos;
+using DSS2_Backend.Models;
 using DSS2_Backend.ViewModels;
 
 namespace DSS2_Backend.Services
 {
     public interface IPasswordService
     {
-        string HashPassword(UserDto user);
+        string HashPassword(User user, UserDto request);
     }
 }
--- DSS2-Backend/DSS2-Backend/Services/PasswordService.cs
+++ DSS2-Backend/DSS2-Backend/Services/PasswordService.cs
@@ -1,13 +1,22 @@
 ﻿using DSS2_Backend.Dtos;
+using DSS2_Backend.Models;
 using DSS2_Backend.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace DSS2_Backend.Services
 {
     public class PasswordService : IPasswordService
     {
-        public string HashPassword(UserDto user)
+        // A random 64-bit salt.
+        private const string Salt = "V8fmyM/+36CprFeSXKXXTTVdnhKnqtTC7ERred3A3XmHh3J3hCZBD2Rn9n1U8ymy";
+
+        public string HashPassword(User user, UserDto request)
         {
-            throw new NotImplementedException();
+            string finalPassword = $"{Salt}{request.Password}{request.Email}";
+
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, finalPassword);
+
+            return hashedPassword;
         }
     }
 }
