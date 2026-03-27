14d633cfa42d734cc0796b70c3708a961c81e80b
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-03-27 17:53:50 +0200

    The LoginResponseDto is now setup.

---
+++ DSS2-Backend/DSS2-Backend/Dtos/LoginResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace DSS2_Backend.Dtos
+{
+    public class LoginResponseDto
+    {
+        public required string AccessToken {  get; set; }
+
+        public required string TokenType { get; set; }
+
+        public int ExpiresInSeconds { get; set; }
+
+        public Dictionary<string,  string> User { get; set; } = new Dictionary<string, string>
+        {
+            {"id", "Required" },
+            {"email", "Required" },
+            {"displayName", "Optional Name" }
+        };
+    }
+}
