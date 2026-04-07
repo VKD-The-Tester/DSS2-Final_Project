1c485162e746377d0ad10752637b362619042d2c
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-04-07 23:57:13 +0300

    Contradicting attribute related metadata is now fixed.

--- DSS2-Backend/DSS2-Backend/Dtos/RegisterRequestDto.cs
+++ DSS2-Backend/DSS2-Backend/Dtos/RegisterRequestDto.cs
@@ -5,14 +5,14 @@ namespace DSS2_Backend.Dtos
 {
     public class RegisterRequestDto
     {
-        [EmailAddress(ErrorMessage = "The email format is invalid."), MaxLength(254), JsonPropertyName("email")]
+        [Required, EmailAddress(ErrorMessage = "The email format is invalid."), MaxLength(254), JsonPropertyName("email")]
         public string Email { get; set; } = null!;
 
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and should be at least eight characters long.")]
-        [JsonPropertyName("password"), Length(6, 128)]
+        [JsonPropertyName("password"), Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{6,128}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and should be at least six characters long.")]
         public string Password { get; set; } = null!;
 
         [JsonPropertyName("displayName")]
-        public string? DisplayName { get; set; }
+        public string? DisplayName { get; set; } = "";
     }
 }
